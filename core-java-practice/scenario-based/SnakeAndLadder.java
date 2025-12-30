cfe7977771a5a46a81e7844c28479d5b776f64a3
pratyushjadonn <pratyush.jadon_cs22@gla.ac.in>
2025-12-30 22:04:16 +0530

    [Pratyush].update UC2 in snake and ladder problem

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -5,5 +5,9 @@ public class SnakeAndLadder {
 		int playerPosition=0;
 		System.out.println("---Welcome to Snake & Ladder Simulator---");
 		System.out.println("Game starts with a single player at position "+playerPosition);
+		//Use Case 2 : Player rolls the dice to get a number between 1 to 6.
+		Random random=new Random();
+		int dice=random.nextInt(6)+1;
+		System.out.println("Dice rolled : "+dice);
 	}
 }

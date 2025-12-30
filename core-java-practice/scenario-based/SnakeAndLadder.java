9d46b67b9ce33267e9669e47c37bbecb8ab89f62
pratyushjadonn <pratyush.jadon_cs22@gla.ac.in>
2025-12-30 16:40:01 +0530

    [Pratyush] .add UC1 of snake and ladder problem

---
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package scenario.based;
+public class SnakeAndLadder {
+	public static void main(String[] args) {
+		//Use Case 1 : Game starts with a single player at position 1
+		int playerPosition=0;
+		System.out.println("---Welcome to Snake & Ladder Simulator---");
+		System.out.println("Game starts with a single player at position "+playerPosition);
+	}
+}

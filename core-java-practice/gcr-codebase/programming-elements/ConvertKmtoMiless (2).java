7ebb608d3e116fd37546674ee29849192ea87743
pratyushjadonn <pratyush.jadon_cs22@gla.ac.in>
2026-01-03 15:20:00 +0530

    Delete: core-java-folder

--- core-java-practice/gcr-codebase/programming-elements/ConvertKmtoMiless (2).java
+++
@@ -1,9 +0,0 @@
-package coreJava;
-import java.util.*;
-public class ConvertKmtoMiles {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		double km= 10.8;
-		System.out.println("10.8 km is:"+(km*1.6)+"miles");
-	}
-}

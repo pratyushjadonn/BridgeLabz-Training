6dbc0e638157401c413fdec46f5fc79c267cf7de
pratyushjadonn <pratyush.jadon_cs22@gla.ac.in>
2026-01-03 15:03:49 +0530

    [Pratyush].refactor my folder structure

--- core-java-practice/gcr-codebase/programming-elements/SideOfSquare.java
+++
@@ -1,9 +0,0 @@
-package coreJava;
-import java.util.*;
-public class SideOfSquare {
-	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		int parameter=scanner.nextInt();   //input the parameter 
-		System.out.println("the side of squre is "+(parameter/4));
-	}
-}
